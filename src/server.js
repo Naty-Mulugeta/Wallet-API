561020a5770e49669fc1cfc229d86425f5c6e21a
Naty-Mulugeta <selamyihun680@gmail.com>
2026-05-23 04:58:23 -0900

    cron.js added and initDB Organized

--- src/server.js
+++ src/server.js
@@ -1,37 +1,27 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import { sql } from './config/db.js'
+import { initDB } from "./config/db.js";
 import rateLimiter from './middleware/ratelimiter.js'
 import transactionsRoute from "./routes/transactionsRoute.js";
+import job from "./config/cron.js";
 
 
 dotenv.config()
 
 const app = express()
 
+if(process.env.NODE_ENV==="production")job.start
+
 app.use(rateLimiter) 
 app.use(express.json())
 
 const PORT = process.env.PORT || 5001
 
-async function initDB() {
-    try {
-        await sql`CREATE TABLE IF NOT EXISTS transactions (
-            id SERIAL PRIMARY KEY,
-            user_id VARCHAR(255) NOT NULL,
-            title VARCHAR(255) NOT NULL,
-            amount DECIMAL(10, 2) NOT NULL,
-            category VARCHAR(255) NOT NULL,
-            created_at DATE NOT NULL DEFAULT CURRENT_DATE
-        )`
 
-        console.log("Database initialized successfully")
 
-    } catch (error) {
-        console.error("Error initializing database:", error)
-        process.exit(1)
-    }
-}
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ status: "OK", message: "Server is healthy" })
+})
 
 app.use("/api/transactions", transactionsRoute);
 
